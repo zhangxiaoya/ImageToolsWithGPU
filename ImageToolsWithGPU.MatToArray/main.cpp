147d0d713baef12607abbf56e4afa697c7169114
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-08-28 16:49:12 +0800

    comment all kernel codes and add one main function

---
+++ ImageToolsWithGPU.MatToArray/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "This is use for put image file to array, and then transfer to GPU memory!" << std::endl;
+	std::cout << "We will not use opencv any more in this file!" << std::endl;
+	system("PAUSE");
+	return 0;
+}
